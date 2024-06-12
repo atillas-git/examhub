936d74d31f09cc1bdcbc4017e36a6ab859b85f41
atillas-git <sina.postaci.tedu@gmail.com>
2024-06-13 02:25:50 +0300

    second xommit

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
             </li>
             <li>
               <Link
-                href="/myExams"
+                href="/myGrades"
                 className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-teal-50 text-white hover:text-gray-800 border-l-4 border-transparent hover:border-teal-900 pr-6"
               >
                 <span className="inline-flex justify-center items-center ml-4">
@@ -61,19 +61,17 @@ const Sidebar = () => {
               </Link>
             </li>
             <li>
-              <li>
-                <Link
-                  href="/myExams"
-                  className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-teal-50 text-white hover:text-gray-800 border-l-4 border-transparent hover:border-teal-900 pr-6"
-                >
-                  <span className="inline-flex justify-center items-center ml-4">
-                    <TbCoin className="font-bold" />
-                  </span>
-                  <span className="ml-2 text-sm tracking-wide truncate">
-                    Billing
-                  </span>
-                </Link>
-              </li>
+              <Link
+                href="/billing"
+                className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-teal-50 text-white hover:text-gray-800 border-l-4 border-transparent hover:border-teal-900 pr-6"
+              >
+                <span className="inline-flex justify-center items-center ml-4">
+                  <TbCoin className="font-bold" />
+                </span>
+                <span className="ml-2 text-sm tracking-wide truncate">
+                  Billing
+                </span>
+              </Link>
             </li>
             <li className="px-5">
               <div className="flex flex-row items-center h-8">
@@ -84,7 +82,7 @@ const Sidebar = () => {
             </li>
             <li>
               <Link
-                href="/market"
+                href="/dataBanks"
                 className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-teal-50 text-white hover:text-gray-800 border-l-4 border-transparent hover:border-teal-900 pr-6"
               >
                 <span className="inline-flex justify-center items-center ml-4">
@@ -129,9 +127,9 @@ const Sidebar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     ></path>
                   </svg>
@@ -155,15 +153,15 @@ const Sidebar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
                     ></path>
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                     ></path>
                   </svg>
@@ -187,9 +185,9 @@ const Sidebar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                     ></path>
                   </svg>
